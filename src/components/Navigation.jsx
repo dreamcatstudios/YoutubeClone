a22a0e648036af973f072f4b38b2bd81bd51ad09
HarshDex <nature4917@gmail.com>
2023-09-03 05:12:35 +0530

    changed the nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,8 +3,15 @@ import component from "../comonent.css"
 function Navigation() {
   return (
     <>
-      <div className="navBar">
+      <div className="navbar">
         <i class="ri-menu-line"></i>
+        <div id="logo">
+          <img src="../src/assets/youtubeIcon.png" alt="" />
+          <h1>YouTube</h1>
+        </div>
+        <div id="search-bar">
+          <input type="text" id = "search" placeholder = "search"/>
+        </div>
       </div>
     </>
   );
