7323065ca4c1ac121ac48d7c736773b06bfce256
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-09-03 03:57:38 +0530

    main

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div>
+      <p style={{ color: "red", fontSize: "2em" }}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, dicta
+        fugiat? Minima ab nostrum repellendus? Id explicabo aliquam reiciendis
+        dolore cum vel, quae sit tempore illum. Veniam quos nesciunt harum.
+      </p>
+    </div>
+  );
+}
+
+export default Navigation;
