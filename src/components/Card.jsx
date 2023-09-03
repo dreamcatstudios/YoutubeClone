8f431e87e11d87b4838b2e7b1ac05614133f99d7
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-09-03 15:05:58 +0530

    fix cards section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import card from "../card.css";
+import "./card.css";
 
 function Card() {
   const cardData = [
@@ -7,7 +7,7 @@ function Card() {
       title: "Coolio - Gangsta's Paradise (Official Music Video) [HD] ft. L.V.",
       channel: "Coolio",
       views: "167K",
-      time: "1 year",
+      time: "1 year ago",
       image: "/src/assets/Drake.jpg",
       icon: "",
     },
@@ -15,7 +15,7 @@ function Card() {
       title: "SUMMER HIGH - AP DHILLON - LYRICAL - [PLAY BEATS]",
       channel: "Play Beats",
       views: "423K",
-      time: "7 day",
+      time: "7 days ago", // Fix typo in "days"
       image: "/src/assets/Coolio.jpg",
       icon: "",
     },
@@ -23,7 +23,7 @@ function Card() {
       title: "Starboy",
       channel: "The Weeknd",
       views: "70M",
-      time: "5 years",
+      time: "5 years ago",
       image: "/src/assets/weekend.jpg",
       icon: "",
     },
@@ -31,15 +31,15 @@ function Card() {
       title: "Superman",
       channel: "EminemMusic",
       views: "57.3M",
-      time: "5 years",
+      time: "5 years ago",
       image: "/src/assets/superman.jpg",
       icon: "",
     },
     {
-      title: "Gods Plan",
+      title: "God's Plan", // Add missing apostrophe
       channel: "Drake",
       views: "28.3M",
-      time: "5 years",
+      time: "5 years ago",
       image: "/src/assets/bcefbc46-6cdf-4cd2-afdd-fec1cfebca7e.jpeg",
       icon: "",
     },
@@ -47,57 +47,49 @@ function Card() {
       title: "Hope",
       channel: "XXXTENTACION",
       views: "28.3M",
-      time: "5 years",
+      time: "5 years ago",
       image: "/src/assets/c0619392-d957-441c-b4f9-607f5254ca25.jpeg",
       icon: "",
     },
   ];
 
   return (
-    <>
+    <div className="card-container">
       {cardData.map((item, index) => (
-        <div key={index} style={{ display: "inline-grid", margin: "0 7px" }}>
-          <div>
-            <img
-              style={{ borderRadius: "10px", width: "350px" }}
-              src={item.image}
-              alt="main-image"
-            />
-          </div>
+        <div key={index} className="card">
+          {" "}
+          {/* Use className instead of Name */}
+          <img
+            className="card-thumbnail"
+            src={item.image}
+            alt="main-image"
+          />{" "}
+          {/* Add "card-thumbnail" class */}
+          <div className="bottom-section">
+            <div className="card-info">
+              <div className="channel-info">
+                <img
+                  src="/src/assets/Drake.jpg"
+                  alt="icon-channel"
+                  className="channel-icon"
+                />
+                <h1 className="card-title">{item.title}</h1>{" "}
+                {/* Display the full title */}
+              </div>
 
-          <div
-            style={{
-              display: "flex",
-            }}
-          >
-            <img
-              src="/src/assets/Drake.jpg"
-              alt="icon-channel"
-              style={{ width: "50px", height: "50px", borderRadius: "100%" }}
-            />
-            <p
-              style={{
-                color: "white",
-                fontSize: "1.5em",
-              }}
-            >
-              {item.title.slice(0, 5)}
-            </p>
-          </div>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <h1 style={{ color: "white", fontSize: "1em" }}>{item.channel}</h1>{" "}
-            <div style={{ display: "flex" }}>
-              <h1
-                style={{ color: "white", fontSize: "1em", padding: "0 20px" }}
-              >
-                {item.views}
-              </h1>
-              <h1 style={{ color: "white", fontSize: "1em" }}>{item.time}</h1>{" "}
+              <div>
+                <h1 className="channel-name">{item.channel}</h1>
+              </div>
+
+              <div className="views-time">
+                <h1>{item.views}</h1>
+                <h1 className="time">{`• ${item.time}`}</h1>
+              </div>
             </div>
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 }
 
