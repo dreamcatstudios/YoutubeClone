9d2fffc27de420b4efc7bcd56e9d0f1a3a2b7a77
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-09-03 05:40:11 +0530

    main

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,56 +4,89 @@ import card from "../card.css";
 function Card() {
   const cardData = [
     {
-      title: "How this works?",
-      para: "This is how this works",
-      channel: "Firstpost",
-      views: "169K",
-      time: "1 day",
-      image: "/src/assets/Coolio.jpg", // Corrected image path
+      title: "Coolio - Gangsta's Paradise (Official Music Video) [HD] ft. L.V.",
+      channel: "Coolio",
+      views: "167K",
+      time: "1 year",
+      image: "",
     },
     {
-      title: "How this not works??",
-      para: "This is how this works",
-      channel: "Firstpost",
-      views: "169K",
-      time: "2 days", // Corrected time
-      image: "/src/assets/Drake.jpg", // Corrected image path
+      title: "SUMMER HIGH - AP DHILLON - LYRICAL - [PLAY BEATS]",
+      channel: "Play Beats",
+      views: "423K",
+      time: "7 day",
+      image: "",
+    },
+    {
+      title: "Starboy",
+      channel: "The Weeknd",
+      views: "70M",
+      time: "5 years",
+      image: "",
+    },
+    {
+      title: "Superman",
+      channel: "EminemMusic",
+      views: "57.3M",
+      time: "5 years",
+      image: "",
+    },
+    {
+      title: "Gods Plan",
+      channel: "Drake",
+      views: "28.3M",
+      time: "5 years",
+      image: "",
+    },
+    {
+      title: "Hope",
+      channel: "XXXTENTACION",
+      views: "28.3M",
+      time: "5 years",
+      image: "",
     },
   ];
 
   return (
     <>
       {cardData.map((item, index) => (
-        <div key={index}>
-          <div className="grid">
-            <div>
-              <img
-                style={{ borderRadius: "10px", width: "350px" }}
-                src={item.image} 
-                alt="main-image"
-              />
-            </div>
+        <div key={index} style={{ display: "inline-grid", margin: "10px" }}>
+          <div>
+            <img
+              style={{ borderRadius: "10px", width: "350px" }}
+              src={"/src/assets/Coolio.jpg"}
+              alt="main-image"
+            />
+          </div>
 
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <img
+              src="/src/assets/Drake.jpg"
+              alt="icon-channel"
+              style={{ width: "50px", height: "50px", borderRadius: "100%" }}
+            />
+            <p
+              style={{
+                color: "white",
+                fontSize: "1.5em",
+              }}
+            >
+              {item.title.slice(0, 5)}
+            </p>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h1 style={{ color: "white", fontSize: "1em" }}>{item.channel}</h1>{" "}
             <div style={{ display: "flex" }}>
-              <img
-                src="/src/assets/Drake.jpg"
-                alt="icon-channel"
-                style={{ width: "50px", height: "50px", borderRadius: "100%" }}
-              />
-              <p style={{ color: "white", fontSize: "1.5em" }}>{item.title}</p>
-            </div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <h1 style={{ color: "white", fontSize: "1em" }}>
-                {item.channel}
-              </h1>{" "}
-              <div style={{ display: "flex" }}>
-                <h1
-                  style={{ color: "white", fontSize: "1em", padding: "0 20px" }}
-                >
-                  {item.views}
-                </h1>
-                <h1 style={{ color: "white", fontSize: "1em" }}>{item.time}</h1>{" "}
-              </div>
+              <h1
+                style={{ color: "white", fontSize: "1em", padding: "0 20px" }}
+              >
+                {item.views}
+              </h1>
+              <h1 style={{ color: "white", fontSize: "1em" }}>{item.time}</h1>{" "}
             </div>
           </div>
         </div>
