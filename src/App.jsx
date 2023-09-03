57f14d148cafa0605edbaa277fb914194595dfeb
HarshDex <nature4917@gmail.com>
2023-09-03 05:36:45 +0530

    sucessfully-added-nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navigation from "./components/Navigation";
 import List from "./components/List";
 import "./App.css";
 import Card from "./components/Card";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navigation/>
       <Card />
     </>
   );
